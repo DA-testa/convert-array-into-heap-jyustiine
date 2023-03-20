69b57c4da3473b35ae909efae61abb4b40f2eee5
Justīne Rostoka <123946301+jyustiine@users.noreply.github.com>
2023-03-20 02:32:14 +0000

    1

--- build_heap.py
+++ build_heap.py
@@ -1,11 +1,33 @@
 # python3
 
+def sift(a, i, swaps):
+    n=len(a)
+    min= i
+    j=2*i+1
+
+    if j<n and a[j]< a[min]:
+        min=j
+    b=2 * i + 2
+    if b<n and a[b]< a[min]:
+        min=b
+
+    if i !=min:
+        swaps.append((i,min))
+
+        a[i], a[min]=a[min], a[i]
+        sift(a, min, swaps)
+
 
 def build_heap(data):
     swaps = []
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
 
+    n = len(data)
+
+    for i in range(n // 2 - 1, -1, -1):
+
+        sift(a, i, swaps)
 
     return swaps
 
@@ -30,7 +52,7 @@ def main():
 
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
-
+    print(len(swaps))
 
     # output all swaps
     print(len(swaps))
