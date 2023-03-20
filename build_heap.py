b69695ae373f8601f10aec357299933c16eefe44
Justīne Rostoka <123946301+jyustiine@users.noreply.github.com>
2023-03-20 02:52:01 +0000

    2

--- build_heap.py
+++ build_heap.py
@@ -1,21 +1,21 @@
 # python3
 
-def sift(a, i, swaps):
+def sift(a,i, swaps):
     n=len(a)
-    min= i
+    min_index= i
     j=2*i+1
 
-    if j<n and a[j]< a[min]:
-        min=j
-    b=2 * i + 2
-    if b<n and a[b]< a[min]:
-        min=b
+    if j<n and a[j]< a[min_index]:
+        min_index=j
+    b=2*i+2
+    if b<n and a[b]< a[min_index]:
+        min_index=b
 
-    if i !=min:
-        swaps.append((i,min))
+    if i !=min_index:
+        swaps.append((i,min_index))
 
-        a[i], a[min]=a[min], a[i]
-        sift(a, min, swaps)
+        a[i], a[min_index]=a[min_index],a[i]
+        sift(a,min_index, swaps)
 
 
 def build_heap(data):
@@ -27,7 +27,7 @@ def build_heap(data):
 
     for i in range(n // 2 - 1, -1, -1):
 
-        sift(a, i, swaps)
+        sift(data, i, swaps)
 
     return swaps
 
@@ -52,7 +52,11 @@ def main():
 
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
-    print(len(swaps))
+
+    if swaps<=4*len(data):
+        print("Number of swaps:",swaps)
+    else:
+        print("Error")
 
     # output all swaps
     print(len(swaps))
